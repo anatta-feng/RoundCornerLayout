2e33cfcf61079d990242792d95097e4aeea4d17a
fxc0719 <1@fxcdev.com>
2018-04-12 10:25:16 +0800

    init commit

---
+++ roundcornerlayout/src/main/java/com/fxc/roundcornerlayout/RoundCornorLinearLayout.kt
@@ -0,0 +1,15 @@
+package com.fxc.roundcornerlayout
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+/**
+ * @author fxc
+ * @date 2018/4/12
+ */
+class RoundCornorLinearLayout : LinearLayout {
+	constructor(context: Context?) : super(context)
+	constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+	constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+}
