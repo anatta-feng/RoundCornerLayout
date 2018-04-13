00345c68367a0f6ee0d0cf8abc86878af3aa1950
fxc0719 <1@fxcdev.com>
2018-04-13 11:07:05 +0800

    添加 RelativeLayout

--- roundcornerlayout/src/main/java/com/fxc/roundcornerlayout/RoundCornerRelativeLayout.kt
+++ roundcornerlayout/src/main/java/com/fxc/roundcornerlayout/RoundCornerRelativeLayout.kt
@@ -5,43 +5,50 @@ import android.graphics.Canvas
 import android.graphics.Path
 import android.graphics.RectF
 import android.util.AttributeSet
-import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import kotlin.math.min
 
 /**
  * @author fxc
- * @date 2018/4/12
+ * @date 2018/4/13
  */
-class RoundCornerLinearLayout : LinearLayout, IRoundCornerLayout {
-	override val boundPath: Path = Path()
-
+class RoundCornerRelativeLayout : RelativeLayout, IRoundCornerLayout {
+	override var isAttached: Boolean = false
 	override val pathArc: RectF = RectF()
 	override val viewBound: RectF = RectF()
 		get() {
 			field.set(paddingLeft.toFloat(), paddingTop.toFloat(), (width - paddingRight).toFloat(), (height - paddingBottom).toFloat())
 			return field
 		}
-
-	override var isAttached: Boolean = false
-
+	override val boundPath: Path = Path()
 	override var roundParams: IRoundCornerLayout.RoundParams = IRoundCornerLayout.RoundParams(this)
 		set(value) {
 			field = value
 			invalidate()
 		}
 
-	override fun getMinSize(): Float {
-		return (min(width, height)).toFloat()
-	}
-
 	override fun makeVpCanDraw() {
 		setWillNotDraw(false)
+	}
+
+	override fun updateView() {
+		invalidate()
+	}
+
+	override fun getMinSize(): Float {
+		return min(width, height).toFloat()
 	}
 
 	constructor(context: Context?) : super(context)
 	constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
 	constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
 		initRoundCornerAttrs(context, attrs)
+	}
+
+	override fun draw(canvas: Canvas) {
+		beforeDraw(canvas)
+		super.draw(canvas)
+		afterDraw(canvas)
 	}
 
 	override fun onAttachedToWindow() {
@@ -52,15 +59,5 @@ class RoundCornerLinearLayout : LinearLayout, IRoundCornerLayout {
 	override fun onDetachedFromWindow() {
 		super.onDetachedFromWindow()
 		isAttached = false
-	}
-
-	override fun draw(canvas: Canvas) {
-		beforeDraw(canvas)
-		super.draw(canvas)
-		afterDraw(canvas)
-	}
-
-	override fun updateView() {
-		invalidate()
 	}
 }
